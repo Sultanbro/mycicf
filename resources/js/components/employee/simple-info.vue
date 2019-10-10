a4cdbd21c8bd1b6d7401cdc258221cc488789752
Johnny <prime.johnny.98@gmail.com>
2019-10-10 12:44:29 +0600

    minor fixes

--- resources/js/components/employee/simple-info.vue
+++ resources/js/components/employee/simple-info.vue
@@ -6,25 +6,25 @@
         </div>
         <div class="flex-row bg-color-blue color-white" @click="reverseCaret()" data-toggle="collapse" data-target="#persons-data" aria-expanded="true">
             <div class="pointer left-menu-nickname-fonts ml-3 mr-3 mt-1 mb-1 jc-sb width100">
-                <span>{{fullname}}</span>
+                <span>{{fullname}}</span><span class="rating">{{rating}}</span>
                 <span class="ml-2 vertical-middle"><i class="fas " :class="caretClass" data-toggle="collapse" href="#multiCollapseExample1"></i></span>
             </div>
         </div>
         <div class="ml-3 mr-3 mt-2 collapse in show" id="persons-data">
             <div class="left-menu-fonts">
-                <span>{{duty}}</span>
+                <span><strong>{{duty}}</strong></span>
             </div>
             <div class="left-menu-fonts">
-                <span>дата рождения: &nbsp; <span>{{birthday}}</span></span>
+                <span><strong>Дата рождения: &nbsp; </strong><span>{{birthday}}</span></span>
             </div>
             <div class="left-menu-fonts">
-                <span>место рождения: &nbsp; <span>{{place_of_birth}}</span></span>
+                <span><strong>Место рождения: &nbsp; </strong><span>{{place_of_birth}}</span></span>
             </div>
             <div class="left-menu-fonts">
-                <span>семейное положение: &nbsp; <span>{{married}}</span></span>
+                <span><strong>Семейное положение: &nbsp; </strong><span>{{married}}</span></span>
             </div>
             <div class="left-menu-fonts">
-                <span>образование: &nbsp; <span>{{education}}</span></span>
+                <span><strong>Образование: &nbsp; </strong><span>{{education}}</span></span>
             </div>
         </div>
         <hr class="left-menu-border mb-0">
@@ -43,6 +43,7 @@
                 place_of_birth: '',
                 married: '',
                 education: '',
+                rating: '',
             }
         },
         props: {
@@ -67,6 +68,7 @@
                     this.married = information.Married === "0" ? "Не указано" : information.Married;
                     this.education = information.Edu  === "0" ? "Не указано" : information.Edu;
                     this.image.encoded = response.result.image;
+                    this.rating = information.Rating === "0" ? "" : information.Rating;
                 }else{
                     alert(response.error);
                 }
