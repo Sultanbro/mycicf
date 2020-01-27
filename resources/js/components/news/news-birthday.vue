fd94e1d25e76df26f7f2e466d1d1a08963f24d36
djumagulov <dimonx715x@yandex.ru>
2020-01-27 11:53:07 +0600

    Доделал день рождения

--- resources/js/components/news/news-birthday.vue
+++ resources/js/components/news/news-birthday.vue
@@ -10,20 +10,25 @@
             <div class="pt-2 pl-4 pr-4 pb-2 relative">
                 <div class="text-center relative">
                     <div>
-                        <div class="absolute height100">
+                        <div class="absolute height100 zi-3">
                             <div class="pr-2 pl-2 pt-2 pb-2 height100 pointer d-flex vertical-middle events-arrow-bg"  v-on:click="events_prev()" v-bind:class="{ visibility_hidden: events_counter === 0 }">
                                 <i class="fa fa-long-arrow-alt-left color-blue" aria-hidden="true"></i>
                             </div>
                         </div>
-                        <div class="absolute height100 absolute-right-0">
+                        <div class="absolute height100 absolute-right-0 zi-3">
                             <div class="pt-2 pr-2 pb-2 pl-2 height100 pointer d-flex vertical-middle events-arrow-bg" v-on:click="events_next()" v-bind:class="{ visibility_hidden: events_counter === 2 }">
                                 <i class="fa fa-long-arrow-alt-right color-blue" aria-hidden="true"></i>
                             </div>
                         </div>
                         <a href="#" v-if="events_counter === 0" class="d-flex flex-column vertical-middle Birthday-margin-inner">
-                            <div class="jc-center d-flex width50">
-                                <img :src="birthday_avatar" class="width100">
+                            <div class="jc-center d-flex width50 events-window-size relative">
+                                <img src="http://animations.shoppinng.ru/wp-content/uploads/2014/02/13.gif" class="absolute width100 height100" />
+                                <img src="/images/balloons-icon.png" class="width100 absolute birthday-balls zi-1">
+                                <img src="/images/wowan.jpg" class="width100">
                             </div>
+<!--                            <div class="width50">-->
+<!--                                <img src="/images/birthday-flags.png" class="width-100-imp birthday-balls">-->
+<!--                            </div>-->
                             <div class="colleagues-info-section__main d-flex flex-column vertical-middle">
                                 <div class="color-blue mt-1 fs-1_1 Birthday-line_height-header text-center">
                                     <span>{{birthday_name}}</span>
@@ -34,9 +39,14 @@
                             </div>
                         </a>
                         <a href="#" v-if="events_counter === 1" class="d-flex flex-column vertical-middle Birthday-margin-inner">
-                            <div class="jc-center d-flex width50">
-                                <img :src="birthday_avatar" class="width100">
+                            <div class="jc-center d-flex width50 events-window-size relative">
+                                <img src="http://animations.shoppinng.ru/wp-content/uploads/2014/02/13.gif" class="absolute width100 height100" />
+                                <img src="/images/balloons-icon.png" class="width100 absolute birthday-balls zi-1">
+                                <img src="/images/wowan.jpg" class="width100">
                             </div>
+                            <!--                            <div class="width50">-->
+                            <!--                                <img src="/images/birthday-flags.png" class="width-100-imp birthday-balls">-->
+                            <!--                            </div>-->
                             <div class="colleagues-info-section__main d-flex flex-column vertical-middle">
                                 <div class="color-blue mt-1 fs-1_1 Birthday-line_height-header text-center">
                                     <span>Ажимов Жанибек Кайратулы</span>
@@ -47,9 +57,15 @@
                             </div>
                         </a>
                         <a href="#" v-if="events_counter === 2" class="d-flex flex-column vertical-middle Birthday-margin-inner">
-                            <div class="jc-center d-flex width50">
+                            <div class="jc-center d-flex width50 events-window-size relative">
+                                <img src="http://animations.shoppinng.ru/wp-content/uploads/2014/02/13.gif" class="absolute width100 height100" />
+<!--                                <img src="/images/wowan.jpg" class="width100">-->
+                                <img src="/images/balloons-icon.png" class="width100 absolute birthday-balls">
                                 <img :src="birthday_avatar" class="width100">
                             </div>
+                            <!--                            <div class="width50">-->
+                            <!--                                <img src="/images/birthday-flags.png" class="width-100-imp birthday-balls">-->
+                            <!--                            </div>-->
                             <div class="colleagues-info-section__main d-flex flex-column vertical-middle">
                                 <div class="color-blue mt-1 fs-1_1 Birthday-line_height-header text-center">
                                     <span>Запопадько Роман Юрьевич</span>
