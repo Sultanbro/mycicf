a244b7d14cec0b8cd367a710b0821ca3ed6509da
bkassymov <bkassymov@cic.kz>
2020-05-28 14:50:56 +0600

    regions cities

---
+++ database/migrations/2020_05_28_161052_create_regions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('isn');
+            $table->text('fullname');
+            $table->integer('parent_isn')->default(0);
+            $table->string('parent_name')->nullable();
+            $table->string('code',50)->default(null);
+            $table->string('numcode',20)->default(null);
+            $table->integer('n_kids')->default(0);
+            $table->timestamps();
+
+            $table->index('id');
+            $table->index('parent_isn');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('regions', function (Blueprint $table){
+            $table->dropIndex('id');
+            $table->dropIndex('parent_isn');
+        });
+        Schema::dropIfExists('regions');
+    }
+}
