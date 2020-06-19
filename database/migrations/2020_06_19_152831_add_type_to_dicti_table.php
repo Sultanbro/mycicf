ba62bb07988e6beef4045cabcfc2131f00b109e8
aakhmedov <akhmet.altynbek@gmail.com>
2020-06-19 15:30:16 +0600

    Сделал онлайн предосмотр

---
+++ database/migrations/2020_06_19_152831_add_type_to_dicti_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeToDictiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dicti', function (Blueprint $table) {
+            $table->string('type', 100)->after('condition_for_property')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dicti', function (Blueprint $table) {
+            //
+        });
+    }
+}
