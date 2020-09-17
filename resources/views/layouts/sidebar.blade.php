cce73c16fcba9d4892f3349e1063e2e44231238d
Yensepbay <yensepbay@gmail.com>
2020-09-17 14:35:59 +0600

    calendar final

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -81,7 +81,7 @@
             <a class="pt-2 pb-2 color-blue font-size-1_2" href="{{route('booking')}}">
                 <li class="leftsidebar-icons">
                     <i class="fa fa-calendar" aria-hidden="true"></i>
-                    <span>Бронирование конференц зала</span>
+                    <span>Бронирование конференц-зала</span>
                 </li>
             </a>
         </ul>
