8cbfea0b1aa19020b6e7619c85558cf8605c72ca
Amadeus <es_252@mail.ru>
2020-10-23 16:10:27 +0600

    Начал рефакторинг шапки

--- resources/views/layouts/refactored/header.blade.php
+++ resources/views/layouts/refactored/header.blade.php
@@ -1,1 +1,13 @@
-<?php
+<header id="header" class="header-wrapper">
+    <div class="container">
+        <div class="header-wrapper__inner">
+            <div class="header-logo">
+                <a href=""></a>
+            </div>
+            <div class="header-menu">
+            </div>
+            <div class="header-auth">
+            </div>
+        </div>
+    </div>
+</header>
