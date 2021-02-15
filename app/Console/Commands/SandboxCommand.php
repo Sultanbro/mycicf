5e2e637816e50cae8cebab77e141c5e5ac05e424
Ilyar <IYusupov@cic.kz>
2021-02-15 16:09:50 +0600

    Added debugbar .gitignore

--- app/Console/Commands/SandboxCommand.php
+++ app/Console/Commands/SandboxCommand.php
@@ -3,7 +3,12 @@
 namespace App\Console\Commands;
 
 use App\Library\Services\KiasServiceInterface;
+use App\Mail\Email;
+use App\User;
 use Illuminate\Console\Command;
+use Illuminate\Mail\Mailable;
+use SebastianBergmann\Comparator\Book;
+use function Deployer\isVeryVerbose;
 
 class SandboxCommand extends Command {
     protected $signature = 'sandbox';
@@ -11,9 +16,6 @@ class SandboxCommand extends Command {
     protected $description = 'Command description';
 
     public function handle() {
-        $res = \Cache::remember('test', 10, function () {
-            return 'string';
-        });
-        dd($res);
+
     }
 }
--- app/Library/Services/Mocks/KiasMock.php
+++ app/Library/Services/Mocks/KiasMock.php
@@ -49,6 +49,7 @@ class KiasMock implements KiasServiceInterface {
     /** Get kias by system credentials
      */
     public function initSystem() {
+        sleep(1); // sleep здесь для имитации задержки
         $this->url = config('kias.url');
 
         $username = config('kias.auth.login');
@@ -161,6 +162,7 @@ class KiasMock implements KiasServiceInterface {
     }
 
     public function getEmplInfo($ISN, $dateBeg, $dateEnd) {
+        sleep(1); // sleep здесь для имитации задержки
         return new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?>
     <data>
         <Mail>mail1@mail.com mail2@mail.com</Mail>
