625e000a704bc948ecf529f635f31c1d900d9328
Ilyar <IYusupov@cic.kz>
2021-02-17 15:41:51 +0600

    Added caching and measuring to NewsController@getPosts

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -145,42 +145,48 @@ class NewsController extends Controller
      * @return array
      */
     public function getPosts(Request $request) {
+        \Debugbar::startMeasure('NewsController@getPosts');
         $user_isn = Auth::user()->ISN;
         $last_index = $request->lastIndex;
-        $response = [];
+        $ttl = now()->addMinutes(10);
+        $key = 'NewsController@getPosts::' . $last_index . '::' . $user_isn;
+        $response = cache()->remember($key, $ttl, function () use ($last_index, $user_isn) {
+            $response = [];
 
-        if($last_index == null){
-            $model = Post::orderBy('id', 'DESC')
-                ->limit(5)
-                ->get();
-        }
-        else {
-            $model = Post::orderBy('id', 'DESC')
-                ->where('id', '<', $last_index)
-                ->limit(5)
-                ->get();
-        }
-        foreach ($model as $item) {
-            $response[] = [
-                'isn' => $item->user_isn,
-                'fullname' => (new User())->getFullName($item->user_isn),
-                'postText' => $item->getText(),
-                'pinned' => $item->pinned,
-                'postId' => $item->id,
-                'edited' => (new Post())->getIsEdited($item->id),
-                'likes' => (new Like())->getLikes($item->id),
-                'isLiked' => (new Like())->getIsLiked($item->id, $user_isn),
-                'date' => date('d.m.Y H:i', strtotime($item->created_at)),
-                'userISN' => $item->user_isn,
-                'comments' => $item->getComments(),
-                'image' => $item->getImage(),
-                'documents' => $item->getDocuments(),
-                'youtube' => $item->getVideo(),
-                'videos' => $item->getVideoUrl(),
-                'post_poll' => $item->getPoll($item->id),
-                'isVoted' => $item->getIsVoted($user_isn, $item->id),
-            ];
-        }
+            if($last_index == null){
+                $model = Post::orderBy('id', 'DESC')
+                    ->limit(5)
+                    ->get();
+            }
+            else {
+                $model = Post::orderBy('id', 'DESC')
+                    ->where('id', '<', $last_index)
+                    ->limit(5)
+                    ->get();
+            }
+
+            // TODO Оптимизировать этот блок тоже
+            foreach ($model as $item) {
+                $response[] = [
+                    'isn' => $item->user_isn,
+                    'fullname' => (new User())->getFullName($item->user_isn),
+                    'postText' => $item->getText(),
+                    'pinned' => $item->pinned,
+                    'postId' => $item->id,
+                    'edited' => (new Post())->getIsEdited($item->id),
+                    'likes' => (new Like())->getLikes($item->id),
+                    'isLiked' => (new Like())->getIsLiked($item->id, $user_isn),
+                    'date' => date('d.m.Y H:i', strtotime($item->created_at)),
+                    'userISN' => $item->user_isn,
+                    'comments' => $item->getComments(),
+                    'image' => $item->getImage(),
+                    'documents' => $item->getDocuments(),
+                    'youtube' => $item->getVideo(),
+                    'videos' => $item->getVideoUrl(),
+                    'post_poll' => $item->getPoll($item->id),
+                    'isVoted' => $item->getIsVoted($user_isn, $item->id),
+                ];
+            }
 
 
 //        $result = [
@@ -189,6 +195,9 @@ class NewsController extends Controller
 //            'post' => $response
 //        ];
 
+            return $response;
+        });
+        \Debugbar::stopMeasure('NewsController@getPosts');
         return $response;
     }
 
