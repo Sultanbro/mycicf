43111ecbf4af2cd2a326d74dac2cf4909839d746
Serabass <serabass565@gmail.com>
2021-03-04 01:34:46 +0600

    GetBirthdaysTest

--- tests/Feature/Site/GetBirthdaysTest.php
+++ tests/Feature/Site/GetBirthdaysTest.php
@@ -2,10 +2,9 @@
 
 namespace Tests\Feature\Site;
 
-use App\Like;
+use App\Branch;
 use App\Post;
-use App\Question;
-use DB;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\Feature\FeatureTestBase;
 use Tests\WithUser;
@@ -21,6 +20,22 @@ class GetBirthdaysTest extends FeatureTestBase {
      */
     private $post;
 
+    /**
+     * @var Branch
+     */
+    private $branch;
+
+    /**
+     * @var Carbon
+     */
+    private $date;
+
+    public function __construct($name = null, array $data = [], $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+
+        $this->date = Carbon::createFromDate(2020, 03, 01);
+    }
+
     public function getRouteName() {
         return 'getBirthdays';
     }
@@ -30,17 +45,34 @@ class GetBirthdaysTest extends FeatureTestBase {
     }
 
     protected function prepare() {
+        $this->branch = new Branch();
+        $this->branch->fullname = 'Test user';
+        $this->branch->kias_id = self::ISN;
+        $this->branch->kias_parent_id = 0;
+        $this->branch->has_child = false;
+        $this->branch->verified = true;
+        $this->branch->duty = 1;
+        $this->branch->birthday = $this->date->addDay();
+        $this->branch->save();
     }
 
     public function handle() {
-        $response = $this->post($this->route);
+        $response = $this->post($this->route, [
+            'd' => $this->date->day,
+            'm' => $this->date->month,
+        ]);
+        $array = collect($response->json('birthdays'));
+        $user = $array->first(function ($entry) {
+            return $entry['ISN'] == self::ISN;
+        });
 
         // TODO Обработать данные
 
         $response->assertStatus(200);
+        $this->assertGreaterThan(0, $array->count());
     }
 
     public function cleanup() {
-
+        $this->branch->delete();
     }
 }
