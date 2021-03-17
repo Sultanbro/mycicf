7e8e6634811a0cd6b3cea25264d1eaa61bee4185
Serabass <serabass565@gmail.com>
2021-03-17 17:20:40 +0600

    wip

--- resources/views/dev/layout.blade.php
+++ resources/views/dev/layout.blade.php
@@ -1,3 +1,18 @@
+@php
+  $routes = [
+    'dev.code' => 'Code',
+    'dev.tests' => 'Tests',
+    'dev.routes' => 'Routes',
+    'dev.vendor' => 'Vendor',
+    'dev.config' => 'Config',
+    'dev.git' => 'Git',
+    'dev.views' => 'Views',
+    'dev.kias' => 'Kias',
+    'dev.caching' => 'Caching',
+    'dev.docs' => 'API Docs',
+];
+@endphp
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,7 +20,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Code analyze</title>
+    <title>Developer area</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
@@ -29,17 +44,12 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <nav class="nav nav-pills flex-column flex-sm-row">
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.code') }}">Code</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.tests') }}">Tests</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.routes') }}">Routes</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.vendor') }}">Vendor</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.config') }}">Config</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.git') }}">Git</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.views') }}">Views</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.kias') }}">Kias</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.caching') }}">Caching</a>
-                <a class="flex-sm-fill text-sm-center nav-link" href="{{ route('dev.docs') }}">API Docs</a>
+            <nav class="nav nav-pills flex-column flex-sm-row pt-2 pb-2">
+                @foreach ($routes as $route => $title)
+                    <a class="flex-sm-fill text-sm-center nav-link {{ Route::currentRouteName() === $route ? 'active' : '' }}" href="{{ route($route) }}">
+                        {{ $title }}
+                    </a>
+                @endforeach
             </nav>
         </div>
     </div>
