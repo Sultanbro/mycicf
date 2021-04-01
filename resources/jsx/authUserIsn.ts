851e21e5596371f2de8108ebe607b1fdb3ab1866
Serabass <serabass565@gmail.com>
2021-04-01 23:58:45 +0600

    Made authUserName and authUserIsn.ts as singleton functions

--- resources/jsx/authUserIsn.ts
+++ resources/jsx/authUserIsn.ts
@@ -1,10 +1,16 @@
 
+let userIsn: string;
+
 export function authUserIsn(): string {
+    if (userIsn) {
+        return userIsn;
+    }
+
     let element = document.getElementById('auth-user-isn');
 
     if (!element) {
         throw new Error('Cannot read ISN');
     }
 
-    return element.innerText;
+    return userIsn = element.innerText;
 }
--- resources/jsx/authUserName.ts
+++ resources/jsx/authUserName.ts
@@ -1,10 +1,16 @@
 
+let userName: string;
+
 export function authUserName(): string {
+    if (userName) {
+        return userName;
+    }
+
     let element = document.getElementById('auth-user-name');
 
     if (!element) {
         throw new Error('Cannot read name');
     }
 
-    return element.innerText;
+    return userName = element.innerText;
 }
