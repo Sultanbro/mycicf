bfe6a51aef931ad7e2844393fb492fa1aa6644f2
DJumagulov <DJumagulov@cic.kz>
2021-04-12 13:44:00 +0600

    wip

--- jest.config.vue.js
+++ jest.config.vue.js
@@ -1,4 +1,7 @@
 module.exports = {
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
     testEnvironment          : 'jsdom',
     "moduleFileExtensions": ["js", "json", "vue"],
     "transform": {
