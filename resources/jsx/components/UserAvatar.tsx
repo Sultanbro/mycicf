519f09c53d260de7bd5862f1201576b4f90d8761
Serabass <serabass565@gmail.com>
2021-04-22 14:44:06 +0600

    Reverted avatar urls

--- resources/jsx/components/UserAvatar.tsx
+++ resources/jsx/components/UserAvatar.tsx
@@ -12,8 +12,8 @@ interface UserAvatarProps {
 
 const DEFAULT_AVATAR = "/images/avatar.png";
 
-let url = (isn: ISN) => `/storage/images/employee/${isn}.png`
-// let url = (isn: ISN) => `https://dummyimage.com/340x340/95bbdf/ffffff.png&text=${isn}`;
+// let url = (isn: ISN) => `/storage/images/employee/${isn}.png`
+let url = (isn: ISN) => `https://dummyimage.com/340x340/95bbdf/ffffff.png&text=${isn}`;
 // let url = (isn: ISN) => DEFAULT_AVATAR;
 
 export function UserAvatar({
