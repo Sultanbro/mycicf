b9f4417c98b178168bd9173139d4a238b762e184
Batyr <bkassymov@cic.kz>
2021-06-08 09:57:48 +0600

    config mail

--- app/Library/Services/SendEmailService.php
+++ app/Library/Services/SendEmailService.php
@@ -26,7 +26,7 @@ class SendEmailService
      */
     public function sendMailRefundStatus($data){    // отправляется после того как ОД подтвердит или откажет в возврате
         $sendMail = Mail::send('emails.mail_refund_status', $data, function ($message) use ($data) {
-            $message->from(env('MAIL_FROM'), env('MAIL_FROM_NAME'));
+            $message->from(config('mail.from.mail_from'), config('mail.username'));
             $message->to($data['email']);
         });
         return $sendMail;
