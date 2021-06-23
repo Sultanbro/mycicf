a8b9c604510bb9bced2e5e6d39859bb5ca0952d3
Serabass <serabass565@gmail.com>
2021-06-23 22:23:47 +0600

    Added SandboxJob for Redis queues testing

--- app/Console/Commands/SandboxCommand.php
+++ app/Console/Commands/SandboxCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Comment;
+use App\Jobs\SandboxJob;
 use App\Library\Services\Kias;
 use App\Library\Services\KiasServiceInterface;
 use App\Mail\Email;
@@ -21,6 +22,6 @@ class SandboxCommand extends Command {
     protected $description = 'Command description';
 
     public function handle() {
-        dd(123);
+        SandboxJob::dispatch();
     }
 }
