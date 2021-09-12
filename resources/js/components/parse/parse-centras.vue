6014d63843ee9827495d0a8f65dae9c4815a7ced
Aziz <es_252@mail.ru>
2021-09-12 17:00:41 +0600

    меняем парсе-центрас

--- resources/js/components/parse/parse-centras.vue
+++ resources/js/components/parse/parse-centras.vue
@@ -1,164 +1,103 @@
 <template>
-    <a-table
-        :columns="columns" :data-source="oracleData" :row-selection="rowSelection"
-        :expanded-row-keys.sync="expandedRowKeys" rowKey="agrempl" :pagination="false">
-        <div
-            slot="filterDropdown"
-            slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
-            style="padding: 8px"
-        >
-            <a-input
-                v-ant-ref="c => (searchInput = c)"
-                :placeholder="`поиск по имени`"
-                :value="selectedKeys[0]"
-                style="width: 188px; margin-bottom: 8px; display: block;"
-                @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
-                @pressEnter="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
-            />
-            <a-button
-                type="primary"
-                icon="search"
-                size="small"
-                style="width: 90px; margin-right: 8px"
-                @click="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
-            >
-                Найти
-            </a-button>
-            <a-button size="small" style="width: 90px" @click="() => handleReset(clearFilters)">
-                Сбросить
-            </a-button>
+    <div>
+        <div class="bg-white pl-3 pr-3 mt-3 box-shadow border-16" >
+            <div class="flex-row jc-sb pr-3 pl-3 vertical-middle flex-row">
+                <div class="width100 table-responsive">
+                    <table class="table table-hover parse-table-topClasses parse-table text-align-center fs-0_8 mb-0">
+                        <thead>
+                        <tr class="border-table-0">
+                            <td colspan="2" class="text-left fs-1_3 pl-0">label</td>
+<!--                            <td class="pt-3" @click="topCompany()"><span :class="viewType === 'market' ? 'pointer parse-active' : 'pointer'">Топ по компаниям</span></td>
+                            <td class="pt-3" @click="topClass()" ><span :class="viewType === 'company' ? 'pointer parse-active' : 'pointer'">Топ по классам</span></td>-->
+                            <td colspan="5" class="text-right border-r-top-16 pt-3">Премии <i class="fa fa-info-circle ml-3"></i></td>
+                            <td></td>
+                            <td colspan="5" class="text-right pt-3">Выплаты <i class="fa fa-info-circle ml-3"></i></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td class="text-left">Компания</td>
+                            <td>{{label_first}}</td>
+                            <td>{{label_second}}</td>
+                            <td>Доля {{label_first}}</td>
+                            <td>Доля {{label_second}}</td>
+                            <td>Изм %</td>
+                            <td>Изм сумма</td>
+                            <td></td>
+                            <td>{{label_first}}</td>
+                            <td>{{label_second}}</td>
+                            <td>Изм %</td>
+                            <td>{{label_first}}</td>
+                            <td>{{label_second}}</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td class="text-left">ДКС</td>
+                            <td class="text-left">{{}}</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td class="text-left">ДСП</td>
+                        </tr>
+                       <tr>
+                           <td></td>
+                           <td></td>
+                           <td class="text-left">ДРПО</td>
+                       </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td class="text-left">ДП</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td class="text-left">Филиалы</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td class="text-left">kupipolis</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td><span class="bold-text">Итого</span></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        <a-icon
-            slot="filterIcon"
-            slot-scope="filtered"
-            type="search"
-            :style="{color: filtered ? '#108ee9' : undefined }"
-        />
-        <template slot="customRender" slot-scope="text, record, index, column">
-      <span v-if="searchText && searchedColumn === column.dataIndex">
-        <template
-            v-for="(fragment, i) in text
-            .toString()
-            .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
-        >
-          <mark
-              v-if="fragment.toLowerCase() === searchText.toLowerCase()"
-              :key="i"
-              class="highlight"
-          >{{ fragment }}</mark
-          >
-          <template v-else>{{ fragment }}</template>
-        </template>
-      </span>
-            <template v-else>
-                {{ text }}
-            </template>
-        </template>
-    </a-table>
+    </div>
 </template>
-<script>
-    const rowSelection = {
-        onChange: (selectedRowKeys, selectedRows) => {
-            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-        },
-        onSelect: (record, selected, selectedRows) => {
-            console.log(record, selected, selectedRows);
-        },
-        onSelectAll: (selected, selectedRows, changeRows) => {
-            console.log(selected, selectedRows, changeRows);
-        },
-    };
 
+<script>
     export default {
-        name:'parse-centras',
+        name: "parse-centras",
         data() {
             return {
-                columns: [
-                    {
-                        title: 'Департамент',
-                        dataIndex: "empl_name",
-                        agrempl: 'agrempl',
-                        width: 400,
-                        scopedSlots: {
-                            filterDropdown: 'filterDropdown',
-                            filterIcon: 'filterIcon',
-                            customRender: 'customRender',
-                        },
 
-                        onFilter: (value, record) =>
-                            record.empl_name
-                                .toString()
-                                .toLowerCase()
-                                .includes(value.toLowerCase()),
-                        onFilterDropdownVisibleChange: visible => {
-                            if (visible) {
-                                setTimeout(() => {
-                                    this.searchInput.focus();
-                                }, 0);
-                            }
-                        },
-                    },
-                    {
-                        title: 'План',
-                        dataIndex: 'feesplan',
-                        agrempl: 'agrempl',
-                    },
-                    {
-                        title: 'Брутто Премии',
-                        dataIndex: 'brutto_prem',
-                        agrempl: 'agrempl',
-                    },
-                    {
-                        title: 'ДСД',
-                        dataIndex: 'dsd',
-                        agrempl: 'agrempl',
-                    },
-                    {
-                        title: 'АВ/Рейтинг',
-                        dataIndex: 'comission_and_rating',
-                        agrempl: 'agrempl',
-                    },
-                    {
-                        title: 'Брутто Выплаты',
-                        dataIndex: 'total_refund_sum',
-                        agrempl: 'agrempl',
-                    },
-                    {
-                        title: 'Нетто Выплаты',
-                        dataIndex: 'netto_refund_sum',
-                        agrempl: 'agrempl',
-                    },
-                ],
-                rowSelection,
-                expandedRowKeys: [],
-                searchText: '',
-                searchInput: null,
-                searchedColumn: '',
-            };
+            }
         },
         props: {
-            oracleData: Array,
-            periods: Object
+            oracleData: Object,
+            periods: Object,
         },
         methods: {
-            handleSearch(selectedKeys, confirm, dataIndex) {
-                confirm();
-                this.searchText = selectedKeys[0];
-                this.searchedColumn = dataIndex;
-            },
-
-            handleReset(clearFilters) {
-                clearFilters();
-                this.searchText = '';
+            textStyle(index){
+                var textStyle = 'bold fs-0_9 bg-grayblue';
+                textStyle = index == 1 || index == 10 || index == 14 ? textStyle : '';
+                return textStyle;
             },
         },
-    };
+    }
 </script>
 
 <style scoped>
-    .anticon svg {
-        margin-bottom: 5px;
-    }
+
 </style>
-
-
