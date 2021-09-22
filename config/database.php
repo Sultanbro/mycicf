24f8ad1049a0c283301676f280b31accc6f361d7
Fallen Starrr <juzopigeon@gmail.com>
2021-09-22 11:14:25 +0600

    changes

--- config/database.php
+++ config/database.php
@@ -92,6 +92,19 @@ return [
         ],
 
     ],
+    'oracle' => [
+        'driver' => 'oracle',
+        'host' => env('ORACLE_DB_HOST', ''),
+        'port' => env('ORACLE_DB_PORT', '1521'),
+        'database' => env('ORACLE_DB_DATABASE', ''),
+        'username' => env('ORACLE_DB_USERNAME', ''),
+        'password' => env('ORACLE_DB_PASSWORD', ''),
+        'charset' => env('ORACLE_DB_CHARSET', 'AL32UTF8'),
+        'prefix' => env('ORACLE_DB_PREFIX', ''),
+        'prefix_schema' => env('ORACLE_DB_SCHEMA_PREFIX', ''),
+        'edition' => env('ORACLE_DB_EDITION', 'ora$base'),
+        'server_version' => env('ORACLE_DB_SERVER_VERSION', '11g'),
+    ],
 
     /*
     |--------------------------------------------------------------------------
